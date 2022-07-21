9822a5c1b596d79afcb7d1a230457de41683334e
Yatharth Pandya <98046678+YatharthPandya@users.noreply.github.com>
2022-07-21 10:20:10 +0530

    Add files via upload

---
+++ task-2(Quiz)/script.js
@@ -0,0 +1,99 @@
+const quizData = [{
+    question: "When is the International Yoga Day celebrated ?",
+    a: "May 11",
+    b: "March 21",
+    c: "April 22",
+    d: "June 21",
+    correct: "d",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "The two-day festival 'North East Calling', is organized by which ministry ?",
+    a: "Ministry of Development of North Eastern Region (DoNER)",
+    b: "Ministry of External Affairs",
+    c: "Ministry of Home Affairs",
+    d: "none of the above",
+    correct: "a",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-111">RESULT</h3>
+        <h3 class="w-100"><br>Correct(s)   : ${correct} </h3>
+        <h3 class="w-100"><br>Incorrect(s) : ${incorrect} </h3>
+        <h3 class="w-100"><br>Final Score  : ${correct-incorrect} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
